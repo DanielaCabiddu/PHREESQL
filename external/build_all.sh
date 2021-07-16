9f6f0128bcc45f40584bbeb88c5d179a40c9d20d
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2021-07-16 13:36:17 +0200

    automatic phreeqc building approach

---
+++ external/build_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+BUILD_FOLDER=${SCRIPT_DIR}/../build
+
+echo ${BUILD_FOLDER}
+mkdir -p ${BUILD_FOLDER} 
+
+echo "Building IPhreeQC ... "
+
+PHREEQC_BUILD_FOLDER=${BUILD_FOLDER}/phreeqc
+
+echo ${PHREEQC_BUILD_FOLDER}
+rm -f ${PHREEQC_BUILD_FOLDER}
+mkdir ${PHREEQC_BUILD_FOLDER}
+cd ${PHREEQC_BUILD_FOLDER}
+cmake ${SCRIPT_DIR}/iphreeqc-3.7.0-15749
+make
+
+echo "Building IPhreeQC ... DONE"
+
+
+
