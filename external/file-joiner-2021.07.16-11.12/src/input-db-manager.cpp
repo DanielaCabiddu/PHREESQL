1eb991dd933ea29983b3f8deaad1919e7af69768
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2022-01-26 12:26:22 +0100

    minor fixes related to metadata table name

--- external/file-joiner-2021.07.16-11.12/src/input-db-manager.cpp
+++ external/file-joiner-2021.07.16-11.12/src/input-db-manager.cpp
@@ -83,7 +83,7 @@ public:
         char **res;
         int row, column;
 
-        query = "SELECT * FROM METADATA WHERE ANALYSIS_ID='" + to_string(ANALYSIS_num) + "';";
+        query = "SELECT * FROM " + metadata_table_name + " WHERE ID='" + to_string(ANALYSIS_num) + "';";
         rc = sqlite3_get_table(db, query.c_str(), &res, &row, &column, &err_message);
         this->queryResult(rc, "selecting data from metadata");
 
