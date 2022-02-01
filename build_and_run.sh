3e026d4d3e3b739925008729b32373757d835263
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2022-02-01 15:57:12 +0100

    improved script build_and_run

--- build_and_run.sh
+++ build_and_run.sh
@@ -8,6 +8,10 @@ rm -rf db.db
 cmake ../phreesqltool
 make
 
-./phreesql -d db_example.db -i ../data/SHORT_SHORT_DB/IN -o ../data/SHORT_SHORT_DB/OUT -m ../data/SHORT_SHORT_DB/META # --export_input --export_output --export_folder ./
+mkdir -p ../data/SHORT_SHORT_DB/OUT_phreeqc
+
+./phreesql -d db_example.db -i ../data/SHORT_SHORT_DB/IN -o ../data/SHORT_SHORT_DB/OUT -m ../data/SHORT_SHORT_DB/META 
+# --export_input --export_output --export_folder ./ --export_id 999 --export_list_ids file_list.txt
+# --run_phreeqc --phreeqc_db /home/danielacabiddu/Devel/src/PHREESQ/llnl.dat
 
 
