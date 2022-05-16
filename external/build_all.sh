a380c7470ce8672f397e8c7107d197cb781804be
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2022-05-16 12:17:13 +0200

    optimized iphreeqc building

--- external/build_all.sh
+++ external/build_all.sh
@@ -12,11 +12,11 @@ echo "Building IPhreeQC ... "
 PHREEQC_BUILD_FOLDER=${BUILD_FOLDER}/phreeqc
 
 echo ${PHREEQC_BUILD_FOLDER}
-rm -f ${PHREEQC_BUILD_FOLDER}
+rm -rf ${PHREEQC_BUILD_FOLDER}
 mkdir ${PHREEQC_BUILD_FOLDER}
 cd ${PHREEQC_BUILD_FOLDER}
 cmake ${SCRIPT_DIR}/iphreeqc-3.7.0-15749
-make
+make -j8
 
 echo "Building IPhreeQC ... DONE"
 
