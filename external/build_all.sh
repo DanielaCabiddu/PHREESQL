63c97df6df32b8040abf1f346465a0337d0b029f
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2022-05-16 12:33:05 +0200

    imrpoved script to build dependencies - includes proj

--- external/build_all.sh
+++ external/build_all.sh
@@ -10,6 +10,7 @@ mkdir -p ${BUILD_FOLDER}
 echo "Building IPhreeQC ... "
 
 PHREEQC_BUILD_FOLDER=${BUILD_FOLDER}/phreeqc
+PROJ_BUILD_FOLDER=${BUILD_FOLDER}/proj
 
 echo ${PHREEQC_BUILD_FOLDER}
 rm -rf ${PHREEQC_BUILD_FOLDER}
@@ -19,6 +20,19 @@ cmake ${SCRIPT_DIR}/iphreeqc-3.7.0-15749
 make -j8
 
 echo "Building IPhreeQC ... DONE"
+echo "Building ProJ ... "
+
+cd ..
+echo ${PROJ_BUILD_FOLDER}
+rm -rf ${PROJ_BUILD_FOLDER}
+mkdir ${PROJ_BUILD_FOLDER}
+cd ${PROJ_BUILD_FOLDER}
+cmake ${SCRIPT_DIR}/proj-osgeo
+make -j8
+
+echo "Building ProJ ... DONE"
+
+
 
 
 
