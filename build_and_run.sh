e62015d55bba9aa40b82ad0863ae990db71c5295
Daniela Cabiddu <daniela.cabiddu@ge.imati.cnr.it>
2022-11-04 13:08:16 +0100

    fix build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -17,4 +17,4 @@ mkdir -p ../data/SHORT_SHORT_DB/OUT_phreeqc
 # --export_input --export_output --export_metadata --export_folder export_test --export_id 10 #--export_list_ids file_list.txt
 # --run_phreeqc --phreeqc_db /home/danielacabiddu/Devel/src/PHREESQ/llnl.dat
 
-./phreesql --epsg_convert --database db_example.db --epsg 3003 --out_filename epsg_3003.csv --out_table table_epsg_3003 --out_database db_epsg_3003
+./phreesql --epsg_convert --database db_example.db --epsg 3003 --out_filename epsg_3003.csv --out_table table_epsg_3003 --out_database db_epsg_3003.db
