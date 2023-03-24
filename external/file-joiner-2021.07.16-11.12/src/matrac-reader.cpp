c8a7133d5803885929413e4ece32e2f186ce4dd5
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2023-03-24 15:26:55 +0100

    copyrights

--- external/file-joiner-2021.07.16-11.12/src/matrac-reader.cpp
+++ external/file-joiner-2021.07.16-11.12/src/matrac-reader.cpp
@@ -1,3 +1,32 @@
+/********************************************************************************
+*  This file is part of PhreeSQL                                                *
+*  Copyright(C) 2023: Daniela Cabiddu                                           *
+*                                                                               *
+*  Author(s):                                                                   *
+*                                                                               *
+*     Daniela Cabiddu (daniela.cabiddu@cnr.it)                                  *
+*                                                                               *
+*     Italian National Research Council (CNR)                                   *
+*     Institute for Applied Mathematics and Information Technologies (IMATI)    *
+*     Via de Marini, 6                                                          *
+*     16149 Genoa,                                                              *
+*     Italy                                                                     *
+*                                                                               *
+*  This program is free software: you can redistribute it and/or modify it      *
+*  under the terms of the GNU General Public License as published by the        *
+*  Free Software Foundation, either version 3 of the License, or (at your       *
+*  option) any later version.                                                   *
+*                                                                               *
+*  This program is distributed in the hope that it will be useful, but          *
+*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   *
+*  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for  *
+*  more details.                                                                *
+*                                                                               *
+*  You should have received a copy of the GNU General Public License along      *
+*  with this program. If not, see <https://www.gnu.org/licenses/>.              *
+*                                                                               *
+*********************************************************************************/
+
 #include <string>
 #include <cstring>
 #include <stdio.h>
