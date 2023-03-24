32532204bc8fa33c7e30370761d735a89dd7e720
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2023-03-24 10:59:18 +0100

    updated build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
+(
+set -e
 
-rm -rf db.db
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BUILD_DIR=${SCRIPT_DIR}/build
+
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+rm -rf *
 cmake ../phreesqltool
 make
 
 cp $PWD/proj/data/proj.db $PWD/
-
 ln -sf $PWD/phreesql $PWD/../Supplementary/phreesql
 
-mkdir -p ../data/SHORT_SHORT_DB/OUT_phreeqc
-
 ./phreesql --fill_db -d db_example.db -i ../data/SHORT_SHORT_DB/IN -o ../data/SHORT_SHORT_DB/OUT -m ../data/SHORT_SHORT_DB/META
+)
+
 # --export_input --export_output --export_metadata --export_folder export_test --export_id 10 #--export_list_ids file_list.txt
 # --run_phreeqc --phreeqc_db /home/danielacabiddu/Devel/src/PHREESQ/llnl.dat
 
