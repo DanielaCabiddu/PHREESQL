b3979f6aed7e47b42d57b2d5e5e4c939357bf7f8
DanielaCabiddu <daniela.cabiddu@ge.imati.cnr.it>
2023-04-07 13:04:45 +0200

    fix fill_db script

--- example/shell_script/00_fill_all_db.sh
+++ example/shell_script/00_fill_all_db.sh
@@ -2,27 +2,32 @@
 
 # 00_fill_all_db.sh MEDIUM [phreeqc|llnl|minteq|wateq4f|sit|ALL]
 
+echo ""
+echo "Running $BASH_SOURCE"
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 SQLDB=$1
 DB=$2
 
 if [ -z "$2" ]
 then
-  echo "Specifica almeno un database di PHREEQC"
+  echo "A PHREEQC database is mandatory and must be provided."
     elif  [ "$2" == "ALL" ] 
   then
     all="phreeqc llnl minteq wateq4f sit"
   else
-    echo "Eseguo $2"
+    echo ""
+    echo "Running PHREESQL for PHREEQC database $2"
+    echo ""
     all=$2
 fi
 
 export dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export phrsql=${dir}/../../build/phreesqlexe
-export db=/Users/zucco/softwarehouse/phreeqcrm-3.7.3-15968/build/database
-export data=./run_DB/${SQLDB}
-
-echo inizio loop
+export db=${dir}/../../external/iphreeqc/database
+export data=${dir}/../run_DB/${SQLDB}
 
 for PHREEQCDB in ${all}
 do
@@ -31,17 +36,16 @@ echo "========= ${PHREEQCDB} ==========="
 
 if [  ${DB} = ALL  ] 
 then
- echo "Vengono creati i database SQLDB_PHREEQCDB  ; es. FULL_minteq.db"
+ #echo "Vengono creati i database SQLDB_PHREEQCDB  ; es. FULL_minteq.db"
  PHREEQCDBALL=${DB}
 
     else
  PHREEQCDBALL=${PHREEQCDB}
- echo "Viene creato 1 database SQLDB_PHREEQCDBALL ; es. FULL_ALL.db "
+ #echo "Viene creato 1 database SQLDB_PHREEQCDBALL ; es. FULL_ALL.db "
  
 fi
-echo fine loop
 
-time ${phrsql} --fill_db -d ./DB/${SQLDB}_${PHREEQCDBALL}.db -i ${data}/IN/${PHREEQCDB} -o ${data}/OUT/${PHREEQCDB} -m ${data}/META/${PHREEQCDB} 
+time ${phrsql} --fill_db -d ${script_dir}/../DB/${SQLDB}_${PHREEQCDBALL}.db -i ${data}/IN/${PHREEQCDB} -o ${data}/OUT/${PHREEQCDB} -m ${data}/META/${PHREEQCDB} 
 
 done
 
